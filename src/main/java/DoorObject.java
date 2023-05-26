4bb1baa1f40395bf1fb30aa5fb2ddc31b345455e
Hyunmaa <fredley.infor@gmail.com>
2023-05-26 15:29:01 +0700

    Test Commit Proyek

---
+++ src/main/java/DoorObject.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+/**
+ * Pacman
+ *
+ * DoorObject class:
+ * Shows a pink rectangle that can only be passed through by a ghost.
+ */
+public class DoorObject extends MapObject {
+    /**
+     * Initialises the door so it can only be entered by ghosts.
+     *
+     * @param position The position on the map.
+     */
+    public DoorObject(Position position) {
+        super(position, MapObject.ObjectType.Door, MapObject.EnterType.GhostOnly);
+    }
+
+    /**
+     * Draws a pink rectangle to fill the position on the grid.
+     *
+     * @param g Reference to the Graphics object for rendering.
+     */
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.PINK);
+        g.fillRect(GamePanel.CELL_DIM*position.x, GamePanel.CELL_DIM* position.y,
+                GamePanel.CELL_DIM, GamePanel.CELL_DIM);
+    }
+}
